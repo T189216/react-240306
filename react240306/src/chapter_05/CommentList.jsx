b8e8c6c45e4dad1a6093c9c0ab63ce9668903997
seoyeon <tus0926@naver.com>
2024-03-07 11:25:33 +0900

    Props로 동적으로 변경하게

--- react240306/src/chapter_05/CommentList.jsx
+++ react240306/src/chapter_05/CommentList.jsx
@@ -4,7 +4,8 @@ import Comment from './Comment';
 function CommentList(props) {
     return (
         <div>
-            <Comment />
+            <Comment name={"Ellie"} comment={"안녕하세요."} />
+            <Comment name={"Isabel"} comment={"반가워요!"} />
         </div>
     );
 }

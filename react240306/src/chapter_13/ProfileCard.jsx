1928f089cc0afa42ba398d8129913264585a16a8
seoyeon <tus0926@naver.com>
2024-03-10 16:21:47 +0900

    Card 컴포넌트

---
+++ react240306/src/chapter_13/ProfileCard.jsx
@@ -0,0 +1,12 @@
+import Card from './Card';
+
+function ProfileCard(props) {
+    return (
+        <Card title="Ellie" backgroundColor="#4ea04e">
+            <p>안녕하세요.</p>
+            <p>리액트 개발자입니다.</p>
+        </Card>
+    );
+}
+
+export default ProfileCard;
